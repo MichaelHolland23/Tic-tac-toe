75a426b495a046853b91bbc3a184a8db16b33862
Mike Holland and Nate McGregor <nate@>
2017-08-24 12:01:47 -0700

    create arrays

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function player(){
+  this.down1 = [0,0,0];
+  this.down2 = [0,0,0];
+  this.down3 = [0,0,0];
+  this.over1 = [0,0,0];
+  this.over2 = [0,0,0];
+  this.over3 = [0,0,0];
+  this.diag1 = [0,0,0];
+  this.diag2 = [0,0,0];
+}
+
+var win1 = [1,2,3];
+var win2 = [4,5,6];
+var win3 = [7,8,9];
+var win4 = [3,5,7];
+var win5 = [1,5,9];
+var win6 = [1,4,7];
+var win7 = [3,6,9];
+var win8 = [2,5,8];
